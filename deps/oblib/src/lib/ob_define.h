9138f16a6d5930ff0dc7ae3951d89db2f44c821d
wangyunlai <hnwyllmm@126.com>
2026-03-19 16:26:48 +0800

    reduce big_chunk_size to 512K

--- deps/oblib/src/lib/ob_define.h
+++ deps/oblib/src/lib/ob_define.h
@@ -1813,7 +1813,11 @@ OB_INLINE bool is_bootstrap_resource_pool(const uint64_t resource_pool_id)
 // ob_malloc & ob_tc_malloc
 const int64_t OB_MALLOC_NORMAL_BLOCK_SIZE = (1LL << 13) - 256;                 // 8KB
 const int64_t OB_MALLOC_MIDDLE_BLOCK_SIZE = (1LL << 16) - 256;                 // 64KB
+#ifdef __ANDROID__
+const int64_t OB_MALLOC_BIG_BLOCK_SIZE = (1LL << 19) - ACHUNK_PRESERVE_SIZE;// 512KB (-17KB)
+#else
 const int64_t OB_MALLOC_BIG_BLOCK_SIZE = (1LL << 21) - ACHUNK_PRESERVE_SIZE;// 2MB (-17KB)
+#endif // __ANDROID__
 const int64_t OB_MALLOC_REQ_NORMAL_BLOCK_SIZE = (240LL << 10);                 // 240KB
 const int64_t WARMUP_MAX_KEY_SET_SIZE_IN_RPC = (1LL << 22);                    // 4M
 const int64_t OB_MAX_CSV_BATCHLINE_BUF_SIZE = (1LL << 23); // 8M
